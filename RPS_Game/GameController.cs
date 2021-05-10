965d9b65267086addadedaefac12230808465d23
Tomkat3370 <72070856+Tomkat3370@users.noreply.github.com>
2021-05-10 20:09:33 +0100

    Attributes added top controller GameChoices enum created

--- RPS_Game/GameController.cs
+++ RPS_Game/GameController.cs
@@ -4,8 +4,36 @@ using System.Text;
 
 namespace RPS_Game
 {
+    public enum GameChoices
+    {
+        Rock,
+        Paper,
+        Scissors
+    }
+
+    /// <summary>
+    /// This will play a rock paper scissors game
+    /// with one player vs. the compurter.
+    /// The game form will do all user input and output.
+    /// </summary>
     public class GameController
     {
+        public string PlayerOneName { get; set; }
+        public string PlayerTwoName { get; set; }
+        public int Rounds { get; set; }
+        public int PlayerOneScore { get; set; }
+        public int PlayerTwoScore { get; set; }
+        public string WinnersName { get; set; }
+        public int CurrentRound { get; set; }
+
+        public GameChoices PlayerOneChoice { get; set; }
+        public GameChoices PlayerTwoChoices { get; set; }
+        
+
+        /// <summary>
+        /// StartGame asks for the players name and how many
+        /// rounds of rock, paper, scissors they want to play.
+        /// </summary>
         public void StartGame()
         {
 
